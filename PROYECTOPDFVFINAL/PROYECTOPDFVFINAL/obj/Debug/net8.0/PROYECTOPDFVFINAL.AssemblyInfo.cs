f41445e8707f5728799d66599d6f733c36d8d684
marant7 <mf2018000597@virtual.upt.pe>
2024-07-14 21:28:03 -0500

    prband

--- PROYECTOPDFVFINAL/PROYECTOPDFVFINAL/obj/Debug/net8.0/PROYECTOPDFVFINAL.AssemblyInfo.cs
+++ PROYECTOPDFVFINAL/PROYECTOPDFVFINAL/obj/Debug/net8.0/PROYECTOPDFVFINAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROYECTOPDFVFINAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3450b5263eaebb04799f8c5135cdfb636ada03f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROYECTOPDFVFINAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROYECTOPDFVFINAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
